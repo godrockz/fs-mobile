ab548990f24b7eeda4cddaf5c15d85550246adbe
Benjamin Jacob <bjb@upsource.de>
2015-03-03 01:49:30 +0100

    * no explicit task loading as it should be done by require('load-grunt-tasks')(grunt);

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt) {
 
     // Load grunt tasks automatically
     require('load-grunt-tasks')(grunt);
-    grunt.loadNpmTasks('grunt-jsonizer');
+    //grunt.loadNpmTasks('grunt-jsonizer'); // this should be done by require('load-grunt-tasks')(grunt);
 
     // Time how long tasks take. Can help when optimizing build times
     require('time-grunt')(grunt);
