6c983d4c5570ec579f9596ee938880ceb30717b0
JoKer <joker777vs.666@gmail.com>
2015-03-10 01:03:19 +0100

    [#5] small improvements for storageManager

--- app/scripts/services/storage_manager.js
+++ app/scripts/services/storage_manager.js
@@ -1,32 +1,35 @@
 
 'use strict';
 angular.module('fsMobile.services')
+
     .service('storageManager', function ($localForage, $http, $q, ENV) {
 
         // url can be with or without domain
         var fetch = function(url) {
             var path = urlToPathConverter(url)
             return $localForage.getItem(path).then(function(value) {
-                console.log('localForage success');
+                console.log('localForage success - ' + path);
                 if (!value) {
                     console.log('but value is empty');
                     value = fetchFromFile(path);
                 }
                 return value;
             }, function(error) {
-                console.log('localForage failed');
+                console.log('localForage failed - ' + path);
                 return fetchFromFile(path);
             });
         };
 
         var fetchFromFile = function(path) {
             return $http.get(path).then(function(response) {
-                console.log('fetching from json file succeeded');
-                if (response.data)
+                console.log('fetching from json file succeeded - ' + path);
+                if (response.data) {
                     $localForage.setItem(path, response.data);
+                    console.log('saving to localForage was successful - ' + path);
+                }
                 return response.data;
             }, function(error) {
-                console.log('fetching from json file failed');
+                console.log('fetching from json file failed - ' + path);
                 return error;
             });
         };
@@ -34,7 +37,10 @@ angular.module('fsMobile.services')
         var urlToPathConverter = function(url) {
             var parser = document.createElement('a')
             parser.href = url;
-            return ENV.offlineJsonDataDirectory + parser.pathname + '.json';
+            var path = parser.pathname;
+            if (path == '/')
+                path = '/index';
+            return ENV.offlineJsonDataDirectory + path + '.json';
         };
 
 
