812cb911b8f13a53ef39a59831cd7e49333e7d36
JoKer <joker777vs.666@gmail.com>
2015-05-11 23:17:32 +0200

    added forgotten app_data.js file

---
+++ app/scripts/services/app_data.js
@@ -0,0 +1,19 @@
+'use strict';
+angular.module('fsMobile.services')
+
+    .factory('AppData', function(Resource) {
+
+        function AppData(data) {
+            angular.forEach(data, function(value, key) {
+            this[key] = (key == '$metaInfo') ? value : new Resource(value);
+            }.bind(this));
+        };
+
+        AppData.prototype = {
+            getLocation: function(el) {
+                return this.locations[el.location];
+            },
+        };
+
+        return AppData;
+    });
