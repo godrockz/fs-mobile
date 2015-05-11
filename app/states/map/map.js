6c8f4e78e1111ee25b6f819d439fb0347f495d8d
srr <srr@upsource.de>
2015-05-11 22:51:22 +0200

    zoom buttons for map/siteplan

--- app/states/map/map.js
+++ app/states/map/map.js
@@ -10,13 +10,26 @@ angular.module('fsMobile.states').config(function ($stateProvider) {
         views: {
             'menuContent': {
                 templateUrl: 'states/map/map.html',
-                controller: function ($scope, $ionicSideMenuDelegate) {
+                controller: function ($scope, $ionicSideMenuDelegate, $ionicScrollDelegate) {
 
                     $ionicSideMenuDelegate.canDragContent(false);
 
                     var headHeight = document.getElementsByClassName('bar-header')[0].offsetHeight;
                     $scope.winHeight = window.innerHeight - headHeight;
                     $scope.winWidth = window.innerWidth;
+
+                    var scrollDelegate = $ionicScrollDelegate.$getByHandle('siteplan');
+                    $scope.zoomlevel = 1;
+
+                    $scope.zoomOut = function(){
+                        $scope.zoomlevel = scrollDelegate.getScrollView().__zoomLevel - 0.5;
+                        scrollDelegate.zoomTo($scope.zoomlevel);
+                    };
+
+                    $scope.zoomIn = function(){
+                        $scope.zoomlevel = scrollDelegate.getScrollView().__zoomLevel + 0.5;
+                        scrollDelegate.zoomTo($scope.zoomlevel);
+                    };
                 }
             }
         }
