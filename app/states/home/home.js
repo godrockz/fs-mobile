4468113c9c8c36f663430e092c0b16ccddabbb09
srr <srr@upsource.de>
2015-06-01 23:18:12 +0200

    first news element design...

--- app/states/home/home.js
+++ app/states/home/home.js
@@ -14,6 +14,30 @@ angular.module('fsMobile.states').config(function ($stateProvider) {
                 templateUrl: 'states/home/home.html',
                 controller: function ($scope,$timeout, $state) {
 
+                    $scope.news = [
+                        {   title: 'Toiletten verstopft!',
+                            text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, ' +
+                            'sed diam nonumy eirmod tempor invidunt ut labore et dolore magna ' +
+                            'aliquyam erat, sed diam voluptua. At vero eos et accusam et justo ' +
+                            'duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata ' +
+                            'sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,' +
+                            ' consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ' +
+                            'ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos ' +
+                            'et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, ' +
+                            'no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+                            date: '2015.08.01 - 20:00'
+                        },
+                        {   title: 'Freibier für alle!',
+                            text: 'Stet clita kasd gubergren, no sea takimata ' +
+                            'sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,' +
+                            ' consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ' +
+                            'ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos ' +
+                            'et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, ' +
+                            'no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+                            date: '2015.08.02 - 12:00'
+                        }
+                    ];
+
                 }
             }
         }
