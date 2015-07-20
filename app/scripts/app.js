9b04b270736b28b9c7d974147cd3063fa7ea6045
Björn Christian Greiff <bgreiff@phoenixcontact.com>
2015-07-20 21:43:42 +0200

    hideKeyboardAccessoryBar commented

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -62,7 +62,7 @@ angular.module('fsMobile', ['ionic', 'tabSlideBox', 'pascalprecht.translate',
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
             if (window.cordova && window.cordova.plugins.Keyboard) {
-                window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                //window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
             }
             if (window.StatusBar) {
                 // org.apache.cordova.statusbar required
