e2a511dcfc330f9c5ce2a8fae70370a67220e112
benni <dodo.phoenix@web.de>
2016-04-26 21:59:02 +0200

    * now - screen

---
+++ app/scripts/services/calendar-grid.js
@@ -0,0 +1,191 @@
+/**
+ * providing a datamodel for favorites and current program views
+ * <p/>
+ * Created by Benjamin Jacob on 26.04.16.
+ * <p/>
+ */
+'use strict';
+angular.module('fsMobile').service('CalendarGrid', function(){
+
+    /**
+     * time function:
+     * if the test is between start and end. (inclusive match)
+     * wheras moment.isBetween is exclusive
+     * @param test - a moment
+     * @param start - a moment
+     * @param end - a moment
+     */
+    function isBetween(test, start, end) {
+        return (test.isAfter(start) ) && (test.isBefore(end) );//|| test.isSame(end) /|| test.isSame(start)
+    }
+
+    /**
+     *
+     * @param height -
+     * @param removeEmptyRows - remove rows without events
+     * @param additionalSteps - add amount of steps to end of the list (allow scroll)
+     * @param onRemovalKeepStepsBetween keep amount steps after an event (only if removeEmptySteps is set tot true)
+     * @param renderAllTimes default:false, if true every step gets its time information rendered
+     * @param showFullGrid - show gridlines on whole screen, default true
+     * @param maxDaysToRender - max days to watch when render. this avoids lot of recursion
+     * @constructor
+     */
+    function CalendarGrid(height, removeEmptyRows, additionalSteps, onRemovalKeepStepsBetween, renderAllTimes, 
+                          useAbsoluteRendering, showFullGrid, maxDaysToRender) {
+        
+        
+        this.useAbsoluteRendering = useAbsoluteRendering === undefined ? true : useAbsoluteRendering;
+        this.showFullGrid = showFullGrid === undefined ? true : showFullGrid;
+        this.events = [];
+        this.firstTime = moment();
+        this.lastTime = moment(0);
+        this.locations = {};
+        this.entryHeight = height || 100; // px
+        this.entryWidth = 90; //percentage
+        this.removeEmptyRows = removeEmptyRows === undefined ? true : removeEmptyRows;
+        this.additionalSteps = additionalSteps || 0;
+        this.onRemovalKeepStepsBetween = onRemovalKeepStepsBetween || 0;
+        this.showAllTimes = renderAllTimes === undefined ? false : renderAllTimes;
+        this.locationCount = 0;
+        this.maxDaysToRender = maxDaysToRender || 10;
+        this.largeTimeSpan = false;
+    }
+
+    /**
+     * time function:
+     * if the test is between start and end. (inclusive match)
+     * wheras moment.isBetween is exclusive
+     * @param test - a moment
+     * @param start - a moment
+     * @param end - a moment
+     */
+    CalendarGrid.prototype.isBetween = isBetween;
+
+    CalendarGrid.prototype.addEvent = function (event) {
+        // add to list of events
+        // update minStart max End
+        // add location if not yet exists
+        this.locations[event.location.id] = event.location;
+        this.locationCount = Object.keys(this.locations).length;
+        this.entryWidth = 99 / (this.locationCount + 1 );// +1 to mind the time column
+
+
+        var start = moment(event.start), end = moment(event.end);
+
+        if (this.firstTime.isAfter(start)) {
+            this.firstTime = start;
+        }
+        if (this.lastTime.isBefore(end)) {
+            this.lastTime = end;
+        }
+        this.events.push(event);
+
+    };
+
+
+    CalendarGrid.prototype.addWorkshopEvent = function (event) {
+        // all ws events should be displayed @ same location so wrap it.
+        var newEvent = angular.copy(event, {});
+        newEvent.location = {id: 'workshop', originalLocation: event.location};
+        this.addEvent(newEvent);
+    };
+
+    CalendarGrid.prototype.getEventsFor = function (startTime, endTime) {
+        var result = [];
+        // TODO: optimize, remind already added events and do not iterate them each time
+        angular.forEach(this.events, function (event) {
+            var start = moment(event.start),
+                end = moment(event.end);
+            if (isBetween(startTime, start, end) || isBetween(endTime, start, end) ||
+                isBetween(start, startTime, endTime) || isBetween(end, startTime, endTime)) {
+                result.push(event);
+            }
+        });
+        return result;
+    };
+
+    CalendarGrid.prototype.getTimeLine = function (stepSizeMin) {
+        var me = this,
+            step = parseInt(stepSizeMin, 10) || 15,
+            currentTime = this.firstTime,
+            result = [],
+            cnt = (this.maxDaysToRender * 24 * 60 / stepSizeMin) + 10 ,// +10 some extra time for event prefix / suffix slots 
+            endTime = this.additionalSteps ? moment(this.lastTime).add(this.additionalSteps * stepSizeMin, 'MINUTE') : this.lastTime,
+            keeptStepCnt = 0,
+            addedEvents = {};
+
+        while (currentTime.isBefore(endTime) && cnt >= 0) {
+            var nextTime = moment(currentTime).add(step, 'MINUTE'),
+                eventsInCurrentTimeSlice = this.getEventsFor(currentTime, nextTime),
+            // prepend n-steps before an event. this way an entry will always display full hour before starting.
+                keepStepsTimeSliceEnd = moment(currentTime).add(1, 'HOUR').set('MINUTE', 0),
+                upcomingEventsInKeepSteps = this.getEventsFor(currentTime, keepStepsTimeSliceEnd),
+
+                isAdditionalTime = currentTime.isAfter(this.lastTime);
+
+            cnt--; // avoid endless loops
+
+            // check if the row should be removed because it hs not entries
+            if (upcomingEventsInKeepSteps.length <= 0 && eventsInCurrentTimeSlice.length <= 0 && this.removeEmptyRows) {
+
+                if (keeptStepCnt >= this.onRemovalKeepStepsBetween && !isAdditionalTime) {
+                    // do not add current time slice
+                    currentTime = nextTime;
+                    continue;
+                }
+                keeptStepCnt++;
+
+            }
+            if (this.removeEmptyRows && upcomingEventsInKeepSteps.length > 0 && eventsInCurrentTimeSlice.length <= 0) {
+                // reset keept step cnt
+                keeptStepCnt = 0;
+            }
+            var locations = {};
+            /* jshint -W083 */
+            angular.forEach(this.locations, function (loc) {
+                locations[loc.id] = {
+                    location: loc,
+                    events: []
+                };
+            });
+
+            /* jshint -W083 */
+            angular.forEach(eventsInCurrentTimeSlice, function (event) {
+
+                var location = locations[event.location.id];
+
+                if (!location) {
+                    throw 'no locationContainer found';
+                }
+
+                if (!addedEvents[event.id]) {
+                    // this is the first event getting added
+                    var durationInMinutes = moment.duration(moment(event.end).diff(moment(event.start))).asMinutes();
+                    var renderHeight = Math.ceil(durationInMinutes / stepSizeMin) * me.entryHeight;
+                    if (location.renderHeight === undefined || location.renderHeight < renderHeight) {
+                        location.renderHeight = renderHeight;
+                    }
+                }
+
+                location.events.push(event);
+                addedEvents[event.id] = event;
+            });
+
+
+            result.push({
+                time: currentTime.toDate(),
+                isFullHour: currentTime.minute() === 0,
+                locations: locations
+            });
+
+            currentTime = nextTime;
+        }
+        if(cnt<=0){
+            this.largeTimeSpan = true;
+        }
+        // create grid starting @ start
+        return result;
+    };
+    
+    return CalendarGrid;
+});
--- app/states/favorites/favorites.js
+++ app/states/favorites/favorites.js
@@ -7,172 +7,7 @@
 'use strict';
 angular.module('fsMobile.states').config(function ($stateProvider) {
 
-    /**
-     * time function:
-     * if the test is between start and end. (inclusive match)
-     * wheras moment.isBetween is exclusive
-     * @param test - a moment
-     * @param start - a moment
-     * @param end - a moment
-     */
-    function isBetween(test, start, end) {
-        return (test.isAfter(start) ) && (test.isBefore(end) );//|| test.isSame(end) /|| test.isSame(start)
-    }
 
-    /**
-     *
-     * @param height -
-     * @param removeEmptyRows - remove rows without events
-     * @param additionalSteps - add amount of steps to end of the list (allow scroll)
-     * @param onRemovalKeepStepsBetween keep amount steps after an event (only if removeEmptySteps is set tot true)
-     * @param renderAllTimes default:false, if true every step gets its time information rendered
-     * @param showFullGrid - show gridlines on whole screen, default true
-     * @param maxDaysToRender - max days to watch when render. this avoids lot of recursion
-     * @constructor
-     */
-    function CalendarGrid(height, removeEmptyRows, additionalSteps, onRemovalKeepStepsBetween, renderAllTimes, useAbsoluteRendering, showFullGrid, maxDaysToRender) {
-        this.useAbsoluteRendering = useAbsoluteRendering === undefined ? true : useAbsoluteRendering;
-        this.showFullGrid = showFullGrid === undefined ? true : showFullGrid;
-        this.events = [];
-        this.firstTime = moment();
-        this.lastTime = moment(0);
-        this.locations = {};
-        this.entryHeight = height || 100; // px
-        this.entryWidth = 90; //percentage
-        this.removeEmptyRows = removeEmptyRows === undefined ? true : removeEmptyRows;
-        this.additionalSteps = additionalSteps || 0;
-        this.onRemovalKeepStepsBetween = onRemovalKeepStepsBetween || 0;
-        this.showAllTimes = renderAllTimes === undefined ? false : renderAllTimes;
-        this.locationCount = 0;
-        this.maxDaysToRender = maxDaysToRender || 10;
-        this.largeTimeSpan = false;
-    }
-
-    CalendarGrid.prototype.addEvent = function (event) {
-        // add to list of events
-        // update minStart max End
-        // add location if not yet exists
-        this.locations[event.location.id] = event.location;
-        this.locationCount = Object.keys(this.locations).length;
-        this.entryWidth = 99 / (this.locationCount + 1 );// +1 to mind the time column
-
-
-        var start = moment(event.start), end = moment(event.end);
-
-        if (this.firstTime.isAfter(start)) {
-            this.firstTime = start;
-        }
-        if (this.lastTime.isBefore(end)) {
-            this.lastTime = end;
-        }
-        this.events.push(event);
-
-    };
-
-
-    CalendarGrid.prototype.addWorkshopEvent = function (event) {
-        // all ws events should be displayed @ same location so wrap it.
-        var newEvent = angular.copy(event, {});
-        newEvent.location = {id: 'workshop', originalLocation: event.location};
-        this.addEvent(newEvent);
-    };
-
-    CalendarGrid.prototype.getEventsFor = function (startTime, endTime) {
-        var result = [];
-        // TODO: optimize, remind already added events and do not iterate them each time
-        angular.forEach(this.events, function (event) {
-            var start = moment(event.start),
-                end = moment(event.end);
-            if (isBetween(startTime, start, end) || isBetween(endTime, start, end) ||
-                isBetween(start, startTime, endTime) || isBetween(end, startTime, endTime)) {
-                result.push(event);
-            }
-        });
-        return result;
-    };
-
-    CalendarGrid.prototype.getTimeLine = function (stepSizeMin) {
-        var me = this,
-            step = parseInt(stepSizeMin, 10) || 15,
-            currentTime = this.firstTime,
-            result = [], 
-            cnt = (this.maxDaysToRender * 24 * 60 / stepSizeMin) + 10 ,// +10 some extra time for event prefix / suffix slots 
-            endTime = this.additionalSteps ? moment(this.lastTime).add(this.additionalSteps * stepSizeMin, 'MINUTE') : this.lastTime,
-            keeptStepCnt = 0,
-            addedEvents = {};
-
-        while (currentTime.isBefore(endTime) && cnt >= 0) {
-            var nextTime = moment(currentTime).add(step, 'MINUTE'),
-                eventsInCurrentTimeSlice = this.getEventsFor(currentTime, nextTime),
-                // prepend n-steps before an event. this way an entry will always display full hour before starting.
-                keepStepsTimeSliceEnd = moment(currentTime).add(1, 'HOUR').set('MINUTE', 0),
-                upcomingEventsInKeepSteps = this.getEventsFor(currentTime, keepStepsTimeSliceEnd),
-
-                isAdditionalTime = currentTime.isAfter(this.lastTime);
-
-            cnt--; // avoid endless loops
-
-            // check if the row should be removed because it hs not entries
-            if (upcomingEventsInKeepSteps.length <= 0 && eventsInCurrentTimeSlice.length <= 0 && this.removeEmptyRows) {
-
-                if (keeptStepCnt >= this.onRemovalKeepStepsBetween && !isAdditionalTime) {
-                    // do not add current time slice
-                    currentTime = nextTime;
-                    continue;
-                }
-                keeptStepCnt++;
-
-            }
-            if (this.removeEmptyRows && upcomingEventsInKeepSteps.length > 0 && eventsInCurrentTimeSlice.length <= 0) {
-                // reset keept step cnt
-                keeptStepCnt = 0;
-            }
-            var locations = {};
-            /* jshint -W083 */
-            angular.forEach(this.locations, function (loc) {
-                locations[loc.id] = {
-                    location: loc,
-                    events: []
-                };
-            });
-
-            /* jshint -W083 */
-            angular.forEach(eventsInCurrentTimeSlice, function (event) {
-
-                var location = locations[event.location.id];
-
-                if (!location) {
-                    throw 'no locationContainer found';
-                }
-
-                if (!addedEvents[event.id]) {
-                    // this is the first event getting added
-                    var durationInMinutes = moment.duration(moment(event.end).diff(moment(event.start))).asMinutes();
-                    var renderHeight = Math.ceil(durationInMinutes / stepSizeMin) * me.entryHeight;
-                    if (location.renderHeight === undefined || location.renderHeight < renderHeight) {
-                        location.renderHeight = renderHeight;
-                    }
-                }
-
-                location.events.push(event);
-                addedEvents[event.id] = event;
-            });
-
-
-            result.push({
-                time: currentTime.toDate(),
-                isFullHour: currentTime.minute() === 0,
-                locations: locations
-            });
-
-            currentTime = nextTime;
-        }
-        if(cnt<=0){
-            this.largeTimeSpan = true;
-        }
-        // create grid starting @ start
-        return result;
-    };
 
     $stateProvider.state('app.favorites', {
         url: '/favorites',
@@ -180,7 +15,7 @@ angular.module('fsMobile.states').config(function ($stateProvider) {
             'menuContent': {
                 templateUrl: 'states/favorites/favorites-list.html',
 
-                controller: function ($scope) {
+                controller: function ($scope, CalendarGrid) {
                     var removeEmptyBlocks = true,
                         cellHeight = 30,
                         slotSizeInMinutes = 15,
@@ -251,4 +86,82 @@ angular.module('fsMobile.states').config(function ($stateProvider) {
             }
         }
     });
+    $stateProvider.state('app.now', {
+        url: '/now',
+        views: {
+            'menuContent': {
+                templateUrl: 'states/favorites/now.html',
+
+                controller: function ($scope, CalendarGrid) {
+
+                    var slotRangeMinutes = 40,
+                        start = moment().add(slotRangeMinutes/2 * -1 ,'MINUTE'),
+                        end = moment().add(slotRangeMinutes/2,'MINUTE');
+
+                    var removeEmptyBlocks = true,
+                        cellHeight = 30,
+                        slotSizeInMinutes = 15,
+                        additionalSteps = 6,
+                        keeptStepCnt = 2,
+                        renderTimeEachStep = false,
+                        useAbsoluteRendering = true,
+                        showFullGrid = true;
+
+                    var grid = new CalendarGrid(cellHeight, removeEmptyBlocks, additionalSteps, keeptStepCnt,
+                        renderTimeEachStep, useAbsoluteRendering, showFullGrid);
+
+                    // select NOW events EVENTS
+                    angular.forEach($scope.appData.program, function (location) {
+                        angular.forEach(location.events, function (event) {
+                            var eStart = moment(event.start),
+                                eEnd = moment(event.end);
+
+                            if(grid.isBetween(eStart, start,end) ||
+                                grid.isBetween(eEnd, start, end) ||
+                                    grid.isBetween( start, eStart, eEnd) ||
+                                    grid.isBetween( end, eStart, eEnd)){
+                                grid.addEvent(event);
+                            }
+                        });
+                    });
+
+
+                    // ### LOCATION SELECTION
+                    function log() {
+                        console.log('idx', $scope.selectedEventIdx, 'event',
+                            $scope.selectedLocationCt.events[$scope.selectedEventIdx]);
+                    }
+
+                    $scope.selectLocation = function (locationCt) {
+                        $scope.selectedLocationCt = locationCt;
+                        $scope.selectedEventIdx = 0;
+                        $scope.selectedEvent = $scope.selectedLocationCt.events[$scope.selectedEventIdx];
+                        log();
+                    };
+                    $scope.nextEvent = function () {
+                        var nextIdx = $scope.selectedEventIdx + 1;
+                        $scope.selectedEventIdx = nextIdx >= $scope.selectedLocationCt.events.length ? 0 : nextIdx;
+                        $scope.selectedEvent = $scope.selectedLocationCt.events[$scope.selectedEventIdx];
+                        log();
+                    };
+                    $scope.previousEvent = function () {
+                        var nextIdx = $scope.selectedEventIdx - 1;
+                        $scope.selectedEventIdx = nextIdx < 0 ? $scope.selectedLocationCt.events.length - 1 : nextIdx;
+                        $scope.selectedEvent = $scope.selectedLocationCt.events[$scope.selectedEventIdx];
+
+                        log();
+                    };
+                    $scope.closeLocation = function () {
+                        delete $scope.selectedLocationCt;
+                    };
+
+
+                    // RESULTS TO SCOPE
+                    $scope.grid = grid;
+                    $scope.timeline = grid.getTimeLine(slotSizeInMinutes);
+                }
+            }
+        }
+    });
 });
+
